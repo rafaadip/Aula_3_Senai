28a17f4607a87f61645b1b5447136cee5912ba4c
rafaadip <39415423+rafaadip@users.noreply.github.com>
2023-09-14 17:01:22 -0300

    Add files via upload

---
+++ aula_3.py
@@ -0,0 +1,58 @@
+# ### 1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número. (O correto é criar uma variavel "quadrado para colocar a conta")
+#  num = int(input("Digite um numero inteiro: "))
+#  quadrado = num * num
+#  print(quadrado)
+
+# ### 1.1
+#  num = int(input("Digite um numero inteiro: "))
+#  calc = num * num 
+#  print("O quadrado de {} é {}" .format(num,calc))
+
+# ### 2 Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+#  nome1 = "Rafael"
+#  nome2 = "Dip"
+#  print(nome1 , nome2)
+
+# ### 2.1
+# nome = input("Digite o seu primeiro nome: ")
+# nome2 = input("Digite o seu sobrenome: ")
+# print(nome,nome2)
+
+### 3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+# num = int(input("Digite o primeiro número inteiro: "))
+# num2 = int(input("Digite o segundo número inteiro: "))
+# print(num,"e", num2)
+
+### 4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+# palavra = "Python"
+# num = input("Digite um número inteiro:")
+# print(palavra + num)
+
+# palavra = "Python"
+# num = input("Digite um número inteiro: ")
+# resultado = palavra + num
+# print(resultado)
+
+### 5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado.
+
+# frase = "Olá mundo ou Hello world"
+# palavra = input("Senhor usuário, digite uma palavra: ")
+# print(frase,palavra)
+
+### 6 -Crie três variáveis para armazenar a quantidade de horas, minutos e segundos. Concatene esses valores para formar uma mensagem de tempo no formato "hh:mm:ss".
+
+# hr = input("Digite as horas: ")
+# min = input("Digite os minutos: ")
+# sec = input("Digite os segundos: ")
+# print(hr + ":" + min + ":" + sec)
+
+### 7 -Declare duas variáveis com números de telefone, incluindo um código de área e o número principal. Concatene esses valores para formar um número de telefone completo.
+n1 = 942088002
+n2 = 991521417
+pais = 55
+cod_area = input("Digite o seu codigo de area: "))
+print(pais,cod_area + n1)
+
+### 8 - Crie uma lista de ingredientes para uma receita. Use concatenação para formar uma única string que liste os ingredientes separados por vírgulas.
+
+### 9 - Peça ao usuário para digitar três adjetivos e armazene-os em variáveis. Em seguida, use essas palavras para criar uma frase concatenada que descreve algo interessante.
